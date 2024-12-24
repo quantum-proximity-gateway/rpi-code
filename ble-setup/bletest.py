53197a8a4427295a2158949435daca5c25c1c0dd
raghav2005 <raghavawasthi2005@gmail.com>
2024-12-24 22:10:50 +0000

    added dotenv for mac addresses

--- ble-setup/bletest.py
+++ ble-setup/bletest.py
@@ -1,5 +1,11 @@
 from bluepy.btle import DefaultDelegate, Scanner, Peripheral, ADDR_TYPE_PUBLIC, ADDR_TYPE_RANDOM
+from dotenv import load_dotenv
 import datetime
+import os
+
+load_dotenv()
+esp_mac_addr = os.getenv("ESP32_MAC_ADDRESS")
+
 SERVICE_UUID = "2246ef74-f912-417f-8530-4a7df291d584"
 CHARACTERISTIC_UUID = "a3445e11-5bff-4d2a-a3b1-b127f9567bb6"
 
@@ -11,7 +17,8 @@ class Key():
 
 keys = []
 addresses = set()
-addresses.add("24:ec:4a:02:54:21")
+addresses.add(esp_mac_addr)
+# addresses.add("24:ec:4a:02:54:21")
 class ScanDelegate(DefaultDelegate):
     def __init__(self):
         DefaultDelegate.__init__(self)
