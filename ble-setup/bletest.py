efd853cc71134742947d29719faa921c5537a754
Marwan <yassinimarwan6@gmail.com>
2025-01-12 22:54:05 +0000

    working version with updated values

--- ble-setup/bletest.py
+++ ble-setup/bletest.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 import datetime
 import os
 from time import sleep
+from collections import defaultdict
 
 load_dotenv()
 esp_mac_addr = os.getenv("ESP32_MAC_ADDRESS")
@@ -19,6 +20,7 @@ class Key():
 keys = []
 addresses = set()
 addresses.add(esp_mac_addr)
+devices = {}
 seenDevices = set()
 # addresses.add("24:ec:4a:02:54:21")
 class ScanDelegate(DefaultDelegate):
@@ -26,27 +28,32 @@ class ScanDelegate(DefaultDelegate):
         DefaultDelegate.__init__(self)
     
     def handleDiscovery(self, dev, isNewDev, isNewData):
-        print(f"Device Seen: {dev.addr}")
-        if isNewDev and dev.addr in addresses:
-            distance = self.calculateDistance(dev.rssi)
-            myKey = Key("", dev.addr, distance)
-            print(f"Distance from key: {distance:.2f}")
+        
+        if dev.addr in addresses:
             mac_address = dev.addr
+            devices[mac_address] = {}
+            distance = self.calculateDistance(dev.rssi)
+            devices[mac_address]['distance'] = distance
+           
             try:
                 with Peripheral(mac_address) as peripheral:
+                    
                     print("CONNECTED!")
                     service = peripheral.getServiceByUUID(SERVICE_UUID)
                     characteristic = service.getCharacteristics(CHARACTERISTIC_UUID)[0]
                     value = characteristic.read().decode("utf-8")
                     print(f"Value: {value}")
-                    seenDevices.add({mac_address: value})
-                    peripheral.disconnect()  
-                    print("Disconnected,")
+                    seenDevices.add(mac_address)
+                    devices[mac_address]['value'] = value
+                    peripheral.disconnect()
+                    
+                    print("Disconnected.")
             except Exception as e:
                 print(f"Error: {e}")
             finally:
                 sleep(2)
-                
+        elif dev.addr in addresses:
+            devices[dev.addr]['distance'] = self.calculateDistance(dev.rssi)
 
     
     def calculateDistance(self, rssi):
