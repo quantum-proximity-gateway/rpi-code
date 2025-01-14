2cd9ce3a2c771a95cd986105af492c3b3387a892
raghav2005 <raghavawasthi2005@gmail.com>
2025-01-14 10:05:21 +0000

    removed unnecessary code from recognise - only checks for a single person (input against folder name in dataset)

--- facial-recognition/recognise.py
+++ facial-recognition/recognise.py
@@ -27,7 +27,7 @@ frame_count = 0
 start_time = time.time()
 fps = 0
 
-def process_frame(frame):
+def process_frame(frame, person_checking_name):
     global face_locations, face_encodings, face_names
     
     # Resize the frame using cv_scaler to increase performance (less pixels processed, less time spent)
@@ -52,61 +52,69 @@ def process_frame(frame):
         if matches[best_match_index]:
             name = known_face_names[best_match_index]
         face_names.append(name)
+
+    person_checking_found = False
+    if person_checking_name in face_names:
+        person_checking_found = True
     
-    return frame
+    return frame, person_checking_found
 
-def draw_results(frame):
-    # Display the results
-    for (top, right, bottom, left), name in zip(face_locations, face_names):
-        # Scale back up face locations since the frame we detected in was scaled
-        top *= cv_scaler
-        right *= cv_scaler
-        bottom *= cv_scaler
-        left *= cv_scaler
+# def draw_results(frame):
+#     # Display the results
+#     for (top, right, bottom, left), name in zip(face_locations, face_names):
+#         # Scale back up face locations since the frame we detected in was scaled
+#         top *= cv_scaler
+#         right *= cv_scaler
+#         bottom *= cv_scaler
+#         left *= cv_scaler
         
-        # Draw a box around the face
-        cv2.rectangle(frame, (left, top), (right, bottom), (244, 42, 3), 3)
+#         # Draw a box around the face
+#         cv2.rectangle(frame, (left, top), (right, bottom), (244, 42, 3), 3)
         
-        # Draw a label with a name below the face
-        cv2.rectangle(frame, (left -3, top - 35), (right+3, top), (244, 42, 3), cv2.FILLED)
-        font = cv2.FONT_HERSHEY_DUPLEX
-        cv2.putText(frame, name, (left + 6, top - 6), font, 1.0, (255, 255, 255), 1)
+#         # Draw a label with a name below the face
+#         cv2.rectangle(frame, (left -3, top - 35), (right+3, top), (244, 42, 3), cv2.FILLED)
+#         font = cv2.FONT_HERSHEY_DUPLEX
+#         cv2.putText(frame, name, (left + 6, top - 6), font, 1.0, (255, 255, 255), 1)
     
-    return frame
+#     return frame
 
-def calculate_fps():
-    global frame_count, start_time, fps
-    frame_count += 1
-    elapsed_time = time.time() - start_time
-    if elapsed_time > 1:
-        fps = frame_count / elapsed_time
-        frame_count = 0
-        start_time = time.time()
-    return fps
+# def calculate_fps():
+#     global frame_count, start_time, fps
+#     frame_count += 1
+#     elapsed_time = time.time() - start_time
+#     if elapsed_time > 1:
+#         fps = frame_count / elapsed_time
+#         frame_count = 0
+#         start_time = time.time()
+#     return fps
 
+person_checking_name = input("Enter the name of the person (case sensitive) who you are searching for: ")
 while True:
     # Capture a frame from camera
     frame = picam2.capture_array()
     
     # Process the frame with the function
-    processed_frame = process_frame(frame)
+    processed_frame, person_checking_found = process_frame(frame, person_checking_name)
+    print(person_checking_found)
+    if person_checking_found:
+        pass # input stuff
     
-    # Get the text and boxes to be drawn based on the processed frame
-    display_frame = draw_results(processed_frame)
+    # # Get the text and boxes to be drawn based on the processed frame
+    # display_frame = draw_results(processed_frame)
     
-    # Calculate and update FPS
-    current_fps = calculate_fps()
+    # # Calculate and update FPS
+    # current_fps = calculate_fps()
     
-    # Attach FPS counter to the text and boxes
-    cv2.putText(display_frame, f"FPS: {current_fps:.1f}", (display_frame.shape[1] - 150, 30), 
-                cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+    # # Attach FPS counter to the text and boxes
+    # cv2.putText(display_frame, f"FPS: {current_fps:.1f}", (display_frame.shape[1] - 150, 30), 
+    #             cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
     
-    # Display everything over the video feed.
-    cv2.imshow('Video', display_frame)
+    # # Display everything over the video feed.
+    # cv2.imshow('Video', display_frame)
     
-    # Break the loop and stop the script if 'q' is pressed
-    if cv2.waitKey(1) == ord("q"):
-        break
+    # # Break the loop and stop the script if 'q' is pressed
+    # if cv2.waitKey(1) == ord("q"):
+    #     break
 
 # By breaking the loop we run this code here which closes everything
 cv2.destroyAllWindows()
