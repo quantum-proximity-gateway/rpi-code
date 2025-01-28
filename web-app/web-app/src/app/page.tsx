fe68830e9ef896121050b376041e364951454a84
Marwan <yassinimarwan6@gmail.com>
2025-01-28 09:38:12 +0000

    Added instructions to what the website should do

--- web-app/web-app/src/app/page.tsx
+++ web-app/web-app/src/app/page.tsx
@@ -1,5 +1,13 @@
 import Image from "next/image";
 
+/*
+  Website to showcase distances to each user, continuously need to call the API to get the latest data.
+  Each user is a circle on a plain background, growing in size as the distance to the user decreases.
+  The user's name is displayed on the circle.
+  If the distance is less than 3m, the circle should be green.
+  If the user is logged in (detected by camera), the circle should say "Logged in", and be blue.
+*/
+
 export default function Home() {
   return (
     <div>
