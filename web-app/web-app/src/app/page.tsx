7712dfaae1f4e142ab9cabaf6d80990b548990ad
Marwan <yassinimarwan6@gmail.com>
2025-01-28 09:43:00 +0000

    added api calls and maps

--- web-app/web-app/src/app/page.tsx
+++ web-app/web-app/src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 /*
   Website to showcase distances to each user, continuously need to call the API to get the latest data.
@@ -9,9 +10,39 @@ import Image from "next/image";
 */
 
 export default function Home() {
+
+  type User = {
+    name: string;
+    distance: number;
+    loggedIn: boolean;
+  }
+
+  const [users, setUsers] = useState<User[]>([]);
+
+  async function getUserData(): Promise<User[]> {
+    const response = await fetch("http://localhost:8000/api/users");
+    const data = await response.json();
+    return data;
+  }
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      getUserData().then(data => setUsers(data));
+    }, 500); // Calling the API every half second to update the user data
+
+    return () => clearInterval(interval);
+  }, []);
+
+
   return (
     <div>
+      {users.map(user => (
+        <div key={user.name}>
+          <p>Name: {user.name}</p>
+          <p>Distance: {user.distance}</p>
+          <p>Logged In: {user.loggedIn ? "Yes" : "No"}</p>
+        </div>
+      ))}
     </div>
-    
   );
 }
