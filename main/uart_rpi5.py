38f6c4c61b261d038f9947bb5af654854754523f
Marwan <yassinimarwan6@gmail.com>
2025-02-07 00:56:50 +0000

    added timeout

--- main/uart_rpi5.py
+++ main/uart_rpi5.py
@@ -1,7 +1,7 @@
 import serial
 import time
 
-ser = serial.Serial('/dev/ttyAMA0') # /dev/ttyS3 for UART3 (help...)
+ser = serial.Serial('/dev/ttyAMA0', timeout=1) # /dev/ttyS3 for UART3 (help...)
 while True:
     print(ser.read(5))
     time.sleep(0.1)
