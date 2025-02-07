c04f0330e43bb478211333f994ae02c400294464
Marwan <yassinimarwan6@gmail.com>
2025-02-07 15:32:33 +0000

    got rid of useless code @RAG+AI

--- main/uart_rpi5.py
+++ main/uart_rpi5.py
@@ -1,11 +1,11 @@
 import serial
 import time
 
-ser = serial.Serial('/dev/ttyAMA0', baudrate=9600, timeout=1) # Specify baud rate and timeout
+ser = serial.Serial('/dev/serial0', baudrate=9600, timeout=1) # Specify baud rate and timeout
 
 try:
     while True:
-        ser.write(b'Hello, UART!\n')  # Send data
+        ser.write(b'Hello!\n')  # Send data
         time.sleep(1)  # Wait for a second before sending the next message
 except serial.SerialException as e:
     print(f"Error: {e}")
