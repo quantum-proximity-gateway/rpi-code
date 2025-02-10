fb887467cf685d6fce54db725ac9ccd45f7c9ff2
Raghav <raghavawasthi2005@gmail.com>
2025-02-10 02:18:37 +0000

    updated main to receive and send - not receiving

--- pico/main.py
+++ pico/main.py
@@ -1,18 +1,28 @@
 import machine
 import time
 
-uart = machine.UART(0, tx=machine.Pin(0), rx=machine.Pin(1), baudrate=115200)
+uart = machine.UART(0, tx=machine.Pin(0), rx=machine.Pin(1), baudrate=115200, timeout=1000)
 led = machine.Pin("LED", machine.Pin.OUT)
 
 print("Pico UART Initialised")
+led.toggle()
 
 while True:
     
-    if uart.any():
-        received = uart.read().decode("UTF-8")
-        print(f"Received from Pi 5: {received}")
-        uart.write(f"Echo: {received}\r\n")
+#    uart.write("Send from Pico\r\n")
+#    print("Sent Pico msg")
     
-    led.toggle()
- 
-    time.sleep(1)
+    try:
+        data = uart.read()
+        if data:
+            print("Data found")
+            data = data.decode('utf-8')
+            print(f"Received from Pi 5: {data}")
+            uart.write(f"Echo: {data}\r\n")
+#        else:
+#            print(f"No data: {data}")
+        
+    except Exception as e:
+        print(f"Error: {e}")
+    
+    time.sleep(2)
