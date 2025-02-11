d6a732df90fdfac23f17aacd6541061166f11e01
Raghav <raghavawasthi2005@gmail.com>
2025-02-11 10:20:08 +0000

    updated main - works both ways (have to hold the cable)

--- pico/main.py
+++ pico/main.py
@@ -1,28 +1,27 @@
 import machine
 import time
 
-uart = machine.UART(0, tx=machine.Pin(0), rx=machine.Pin(1), baudrate=115200, timeout=1000)
+uart = machine.UART(0, tx=machine.Pin(0), rx=machine.Pin(1), baudrate=115200)
 led = machine.Pin("LED", machine.Pin.OUT)
 
 print("Pico UART Initialised")
 led.toggle()
 
 while True:
-    
-#    uart.write("Send from Pico\r\n")
-#    print("Sent Pico msg")
+    uart.write("Send from Pico\r\n")
+    print("Sent Pico msg")
     
     try:
-        data = uart.read()
-        if data:
-            print("Data found")
-            data = data.decode('utf-8')
-            print(f"Received from Pi 5: {data}")
-            uart.write(f"Echo: {data}\r\n")
-#        else:
-#            print(f"No data: {data}")
+       data = uart.read()
+       if data:
+           print("Data found")
+           data = data.decode('utf-8')
+           print(f"Received from Pi 5: {data}")
+           uart.write(f"Echo: {data}\r\n")
+       else:
+           print(f"No data: {data}")
         
     except Exception as e:
-        print(f"Error: {e}")
+       print(f"Error: {e}")
     
     time.sleep(2)
