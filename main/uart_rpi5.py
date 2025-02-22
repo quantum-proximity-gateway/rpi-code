5737a8c1130833810c9895cf43dafdea50d0e440
Marwan <yassinimarwan6@gmail.com>
2025-02-22 17:47:36 +0000

    uart script changed

--- main/uart_rpi5.py
+++ main/uart_rpi5.py
@@ -1,14 +1,24 @@
 import serial
 import time
 
-ser = serial.Serial('/dev/serial0', baudrate=9600, timeout=1) # Specify baud rate and timeout
 
-try:
-    while True:
-        ser.write(b'Hello!\n')  # Send data
-        time.sleep(1)  # Wait for a second before sending the next message
-except serial.SerialException as e:
-    print(f"Error: {e}")
-finally:
-    if ser.is_open:
-        ser.close()
+def write_to_pico(username, password):
+    ser = serial.Serial('/dev/ttyAMA0', baudrate=9600, timeout=1) # Specify baud rate and timeout
+    try:
+        while True:
+            json_data = f'{{"username": "{username}", "password": "{password}"}}\n'
+            ser.write(json_data.encode())
+            response = ser.readline() 
+            if response:
+                # decode the bytes into a string and strip any trailing newline/carriage return
+                decoded_response = response.decode('utf-8').strip()
+                print("Received:", decoded_response)
+                if decoded_response == "OK":
+                    print('Logged In')
+                    return
+            time.sleep(1)  # Wait for a second before repeating
+    except serial.SerialException as e:
+        print(f"Error: {e}")
+    finally:
+        if ser.is_open:
+            ser.close()
