ac6bf3bccd19407e4fa2cac3781633bca3544867
Abdul Muhaymin Abdul Hafiz <mr-abdul@live.co.uk>
2025-02-25 08:50:52 +0000

    Added explanation for how to setup cron job

--- update_and_train.sh
+++ update_and_train.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd #RPI CODE REPO DIRECTORY
+cd #/path/to/repo/
 
 git fetch origin main
 
@@ -14,3 +14,13 @@ if ! git diff --quiet HEAD origin/main; then
 else
 	echo "No updates found."
 fi
+
+# To schedule this script to run automatically, add a new cron job by running `crontab -e`, and paste the following at the end:
+#
+# ```
+# */1 * * * * /path/to/repo/update_and_train.sh >> /path/to/repo/update.log 2>&1
+# ```
+#
+# This will:
+# - run the script every 1 minute
+# - log output to update.log for debugging
